ea2a8233badc9f10cda38df25144837ae10689b5
Randy <linyejoe2@gmail.com>
2022-09-19 13:18:53 +0800

    follow test

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -7,13 +7,24 @@ npm run build # 生成靜態檔案
 cd ./dist # 進入生成的資料夾
 
 # deploy to github
-msg='來自 github action 的自動部署。'
+if [ -z "$GITHUB_TOKEN" ]; then
 
-githubUrl=https://linyejoe2:${GITHUB_TOKEN}@github.com/linyejoe2/tic-tac-toe.lamdo.git
+  msg='deploy'
 
-git config --global user.name "linyejoe2"
+  githubUrl=git@github.com:linyejoe2/tic-tac-toe.lamdo.git
 
-git config --global user.email "linyejoe2@gmail.com"
+else
+
+  msg='來自 github action 的自動部署。'
+
+  githubUrl=https://linyejoe2:${GITHUB_TOKEN}@github.com/linyejoe2/tic-tac-toe.lamdo.git
+
+  git config --global user.name "linyejoe2"
+
+  git config --global user.email "linyejoe2@gmail.com"
+
+fi
+
 
 git init
 
