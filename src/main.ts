18157ebed8c4bdd86e87006ed260402d163c150b
Hundo1018 <f74169@gmail.com>
2022-09-30 15:32:27 +0800

    螃蟹掰

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./style.css";
-import { Application, TextureSystem } from 'pixi.js';
-import Crab from "./Crab";
+import { Application } from 'pixi.js';
 import Game from "./Game/GameView";
 const app = new Application({
   width: 100,// 寬度
@@ -10,13 +9,10 @@ const app = new Application({
 
 document.querySelector("#app")?.append(app.view);
 
-const crab1 = new Crab({ x: 50, y: 50 });
 const game1 = new Game(app,true);
 // Listen for frame updates
 app.ticker.add(() => {
   app.stage.removeChildren();
-  crab1.render(app);
   game1.render(app);
-  crab1.update(app.ticker.deltaMS);
 
 });
